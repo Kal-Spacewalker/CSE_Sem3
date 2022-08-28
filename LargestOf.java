75042cf3f9f46d9cd8179ba049b7f96c35680145
Kal-Spacewalker <112195307+Kal-Spacewalker@users.noreply.github.com>
2022-08-28 17:01:16 +0530

    Add files via upload

---
+++ LargestOf.java
@@ -0,0 +1,13 @@
+class LargestOf
+{
+	public static void main(String args[])
+	{
+		int a,b,c,max;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		c=Integer.parseInt(args[2]);
+		max=(a>b)?((a>c)?a:c):((b>c)?b:c);
+		System.out.println(max+" is largest of the three numbers");
+	}
+}
+
