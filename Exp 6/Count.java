23379d273fa48cd85348b30b88d22dfba76e1662
Kal-Spacewalker <112195307+Kal-Spacewalker@users.noreply.github.com>
2022-09-25 19:58:36 +0530

    Update Count.java

--- Exp 6/Count.java
+++ Exp 6/Count.java
@@ -4,14 +4,14 @@ class Count
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
-		int ab=0,uc=0,lc=0,wspc=0,dg=0,sc=0;
-		System.out.println("Enter a multiword string: \n");
+		int ab=0,uc=0,lc=0,wsp=0,dg=0,spc=0;
+		System.out.println("Enter a multiword string: ");
 		String s=sc.nextLine();
 		for(int i=0;i<s.length();i++)
 		{
 			if(Character.isLetter(s.charAt(i)))
 			{
-				az++;
+				ab++;
 				if(Character.isUpperCase(s.charAt(i)))
 					uc++;
 				else if(Character.isLowerCase(s.charAt(i)))
@@ -20,15 +20,15 @@ class Count
 			else if(Character.isDigit(s.charAt(i)))
 				dg++;
 			else if(Character.isWhitespace(s.charAt(i)))
-				wspc++;
+				wsp++;
 			else
-				sc++;
+				spc++;
 		}
-		System.out.println("Alphabets= "+ab);
-		System.out.println("Uppercase= "+uc);
-		System.out.println("Lowercase= "+lc);
-		System.out.println("Digits= "+dg);
-		System.out.println("Whitespace= "+wspc);
-		System.out.println("Special characters= "+sc);
+		System.out.println("\nAlphabets= "+ab);
+		System.out.println("\nUppercase= "+uc);
+		System.out.println("\nLowercase= "+lc);
+		System.out.println("\nDigits= "+dg);
+		System.out.println("\nWhitespace= "+wsp);
+		System.out.println("\nSpecial characters= "+spc);
 	}
 }
