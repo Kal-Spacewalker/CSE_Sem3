a57d52f6dee6742e54cbc5b78107d97aebec7d83
Kal-Spacewalker <112195307+Kal-Spacewalker@users.noreply.github.com>
2022-09-25 20:17:50 +0530

    Update DecToBin.java

--- Exp 6/DecToBin.java
+++ Exp 6/DecToBin.java
@@ -4,17 +4,18 @@ class DecToBin
 	public static void main(String args[])
 	{
 		int[] rem=new int[50];
-		int n,i=0,j;
+		int n,i=0,j,num;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter a number: ");
 		n=sc.nextInt();
+		num=n;
 		while(n!=0)
 		{
 			rem[i]=n%2;
 			n=n/2;
 			i++;
 		}
-		System.out.print("Binary equivelent of "+n+" n is: ");
+		System.out.print("Binary equivelent of "+num+" is: ");
 		for(j=i-1;j>=0;j--)
 		{
 			System.out.print(rem[j]);
