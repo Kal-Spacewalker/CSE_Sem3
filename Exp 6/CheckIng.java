688bdfefc583e7dd22004e4dc4bc2aab5753270f
Kal-Spacewalker <112195307+Kal-Spacewalker@users.noreply.github.com>
2022-09-25 19:50:29 +0530

    Update CheckIng.java

--- Exp 6/CheckIng.java
+++ Exp 6/CheckIng.java
@@ -11,12 +11,12 @@ class CheckIng
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a string: \n");
+		System.out.println("Enter a string: ");
 		String s=sc.next();
 		boolean c=CheckIng.check(s);
 		if(c)
-			System.out.println(s+" ends with ing");
+			System.out.println("\n"s+" ends with ing");
 		else
-			System.out.println(s+" does not end with ing");
+			System.out.println("\n"s+" does not end with ing");
 	}
 }
